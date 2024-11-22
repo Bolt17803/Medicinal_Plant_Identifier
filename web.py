f2d8d9ff7afbc933c2e4aef564276ab6e088b562
Bolt17803 <anumulacs@iitbhilai.ac.in>
2024-11-22 10:26:29 +0000

    uodated re and web.py

--- web.py
+++ web.py
@@ -9,6 +9,9 @@ import gzip
 from torchvision.models import resnet18
 import torch.nn.functional as F
 import torch.nn as nn
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
 
 class CNN(nn.Module):
     def __init__(self):
@@ -53,6 +56,57 @@ print("Model weights loaded successfully.")
 with gzip.open('mean_embeddings.pkl.gz', 'rb') as f:
     mean_embeddings = pickle.load(f)
 
+def get_medicinal_info_from_google(plant_name):
+    query = f"{plant_name} medicinal uses"
+    # Use start and stop to get the top result (start=0 means the first page of results)
+    results = list(search(query, num_results=2))  # This will fetch the top 1 result
+
+    if results:
+        return results[0]  # Return the URL of the top search result
+    else:
+        return "No results found."
+    
+def get_data(plant_name):
+    link=get_medicinal_info_from_google(plant_name)
+    print(link)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(link, headers=headers)
+    if response.status_code == 200:
+        page_content = response.text
+        print("Page fetched successfully!")
+        soup = BeautifulSoup(page_content, "html.parser")
+    else:
+        print(f"Failed to fetch the webpage. Status code: {response.status_code}")
+
+    uses_component = soup.find('div', class_='uses-container')
+
+    if uses_component:
+        t1=uses_component.get_text(strip=True)
+    else:
+        t1=None
+        print("The uses web component was not found.")
+    
+    side_effects_component = soup.find('div', class_='side-effects-page')
+
+    if side_effects_component:
+        t2=side_effects_component.get_text(strip=True)
+    else:
+        t2=None
+        print("The uses web component was not found.")
+
+    precautions_component = soup.find('div', class_='precautions-page')
+
+    if precautions_component:
+        t3=precautions_component.get_text(strip=True)
+    else:
+        t3=None
+        print("The uses web component was not found.")
+
+    return link,t1,t2,t3
+
 name_mapper={0:'ajwain',1:'aswagandha',2:'black pepper',3:'cassia fistula',4:'elaichi',5:'gadhur',6:'ixora coccinea',7:'kalanchoe pinata',8:'LAGERSTROEMIA',9:'lawang',10:'MAGNOLIA CHAMPACA',11:'mimosa',12:'neem',13:'S6[unk]',14:'S7[unk]',15:'spathodea',16:'betel',17:'centella asiatica',18:'merremia_peltata'}
 def main():
     st.title("Image Classification with Streamlit")
@@ -92,6 +146,17 @@ def main():
 
         # Show the predicted class
         st.write(f"Predicted Class: {name_mapper[assigned_class]}")
+
+        link, uses, side_effects, precautions = get_data(name_mapper[assigned_class])
+        
+        if uses!=None:
+            st.write(f"Plants Uses: {uses}")
+        if side_effects!=None:
+            st.write(f"Side-Effects: {side_effects}")
+        if precautions!=None:
+            st.write(f"Precautions: {precautions}")
+
+        st.write(f'Check this link for more plant info:{link}')
 
 
 if __name__ == "__main__":
